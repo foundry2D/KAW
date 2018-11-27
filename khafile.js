c4a827fe32d91f7ec907e34d984169d56c5152be
Unknown <bastiennadeau@gmail.com>
2018-11-27 18:17:15 -0500

    Started audio engine

--- khafile.js
+++ khafile.js
@@ -2,4 +2,6 @@ let project = new Project('KAW');
 project.addAssets('Assets/**');
 project.addShaders('Shaders/**');
 project.addSources('Sources');
+project.addLibrary('zui');
+project.addLibrary('ae');
 resolve(project);
